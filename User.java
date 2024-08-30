5309b71a0f0b165ca7c54fccb0e1de5cfcf1d383
maheenrizwanfya <maheenrizwanfya@gmail.com>
2024-08-30 21:59:16 +0500

    Update User.java

--- User.java
+++ User.java
@@ -1,25 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class User {
     String userId;
     String name;
     double balance;
-    List<Payment> paymentHistory;
 
     public User(String userId, String name, double balance){
         this.userId = userId;
         this.name = name;
         this.balance = balance;
-        paymentHistory = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUserId() {
-        return userId;
     }
 
     public double getBalance() {
@@ -28,9 +15,5 @@ public class User {
 
     public void setBalance(double balance) {
         this.balance = balance;
-    }
-
-    public List<Payment> getPaymentHistory() {
-        return paymentHistory;
     }
 }
